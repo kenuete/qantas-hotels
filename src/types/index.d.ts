02f341b9eb2ac692bf76eedd238e6ae84a9b6aa5
kenuete <kenuete@gmail.com>
2025-02-14 19:19:34 +1100

    define typescript type for the HotelResults

---
+++ src/types/index.d.ts
@@ -0,0 +1,51 @@
+type HotelData = {
+    hotels: Hotel[];
+  }
+  
+  type Hotel = {
+    id: string;
+    property: Property;
+    offer: Offer;
+  }
+  
+  type Property = {
+    propertyId: string;
+    title: string;
+    address: string[];
+    previewImage: Image;
+    rating: Rating;
+  }
+  
+  type Image = {
+    url: string;
+    caption: string;
+    imageType: string;
+  }
+  
+  type Rating = {
+    ratingValue: number;
+    ratingType: string;
+  }
+  
+  type Offer = {
+    promotion: Promotion;
+    name: string;
+    displayPrice: Price;
+    savings: Price | null;
+    cancellationOption: CancellationOption;
+  }
+  
+  type Promotion = {
+    title: string;
+    type: string;
+  }
+  
+  type Price = {
+    amount: number;
+    currency: string;
+  }
+  
+  type CancellationOption = {
+    cancellationType: string;
+  }
+  
