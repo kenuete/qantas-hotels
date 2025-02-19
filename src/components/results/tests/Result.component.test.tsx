f06fd7f5326b855249efae66ebc6937cede5ec53
kenuete <kenuete@gmail.com>
2025-02-19 16:24:07 +1100

    unit tests

--- src/components/results/tests/Result.component.test.tsx
+++ src/components/results/tests/Result.component.test.tsx
@@ -4,19 +4,19 @@ import { Hotel } from '../../../types'
 
 // Mocks
 jest.mock('../../property/Image.component', () => {
-    return {
-        default: () => <div data-testid="image-test-id">Image Component</div>
-    }
+  return {
+    default: () => <div data-testid='image-test-id'>Image Component</div>,
+  }
 })
 jest.mock('../../property/Details.component', () => {
-    return {
-        default: () => <div data-testid="details-test-id">Details Component</div>
-    }
+  return {
+    default: () => <div data-testid='details-test-id'>Details Component</div>,
+  }
 })
 jest.mock('../../property/Pricing.component', () => {
-    return {
-        default: () => <div data-testid="pricing-test-id">Pricing Component</div>
-    }
+  return {
+    default: () => <div data-testid='pricing-test-id'>Pricing Component</div>,
+  }
 })
 
 describe('Result Component', () => {
@@ -68,4 +68,41 @@ describe('Result Component', () => {
     expect(screen.getByTestId('pricing-test-id')).toBeInTheDocument()
   })
 
+  it('handles missing row prop gracefully', () => {
+    // @ts-expect-error - Testing behavior with an undefined row
+    render(<Result id='missing-row' row={undefined} />)
+
+    // Check if the component still renders without throwing an error
+    expect(screen.getByTestId('result-row-missing-row')).toBeInTheDocument()
+  })
+
+  it('handles missing property in row', () => {
+    const testId = 'no-property'
+    // @ts-expect-error - Testing behavior with an undefined properties
+    const mockHotelWithoutProperty = {
+      id: testId,
+      property: undefined,
+      offer: mockHotel.offer,
+    } as Hotel
+
+    render(<Result id={testId} row={mockHotelWithoutProperty} />)
+
+    expect(screen.getByTestId(`result-row-${testId}`)).toBeInTheDocument()
+    expect(screen.getByTestId('image-test-id')).toBeInTheDocument() 
+  })
+
+  it('handles missing offer in row', () => {
+    const testId = 'no-offer'
+    // @ts-expect-error - Testing behavior with an undefined values
+    const mockHotelWithoutOffer = {
+      id: testId,
+      property: mockHotel.property,
+      offer: undefined,
+    } as Hotel
+
+    render(<Result id={testId} row={mockHotelWithoutOffer} />)
+
+    expect(screen.getByTestId(`result-row-${testId}`)).toBeInTheDocument()
+    expect(screen.getByTestId('pricing-test-id')).toBeInTheDocument()
+  })
 })
